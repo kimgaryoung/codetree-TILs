d39bd1d6fa0d0e0a585a7c4d9825b67dc8480667
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2024-08-02 23:42:08 +0900

    Title: 좌우 지그재그 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(i%2==0)
+            {
+
+                cout<<i*n+j+1<<" ";
+            }    
+            else if (i%2!=0)
+            {
+               cout<<(i+1)*n-j<<" ";
+            }
+               
+               
+            
+        }
+
+        cout<<"\n";
+    }
+    return 0;
+}
