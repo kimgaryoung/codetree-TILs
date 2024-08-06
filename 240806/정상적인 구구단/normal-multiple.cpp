5f4a19364583d993c27a1e1ac0459fc442e87628
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2024-08-06 20:37:36 +0900

    Title: 정상적인 구구단, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/정상적인 구구단/normal-multiple.cpp
+++ 240806/정상적인 구구단/normal-multiple.cpp
@@ -11,7 +11,7 @@ int main() {
         for(int j=1; j<=n; j++)
         {
             cout<<i<<" * "<<j<<" = "<<i*j;
-            if(j<3)
+            if(j<n)
             {
                 cout<<", ";
             }
