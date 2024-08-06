d3de5aded355c2ee6b06dad9461a5e5cc3983760
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2024-08-06 20:37:11 +0900

    Title: 정상적인 구구단, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240806/정상적인 구구단/normal-multiple.cpp
+++ 240806/정상적인 구구단/normal-multiple.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    for(int i=1; i<4; i++)
+
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++)
     {
-        for(int j=1; j<4; j++)
+        for(int j=1; j<=n; j++)
         {
             cout<<i<<" * "<<j<<" = "<<i*j;
             if(j<3)
