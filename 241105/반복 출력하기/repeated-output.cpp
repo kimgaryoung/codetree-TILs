b599d9647f118ea7bc647ad147afe8662437d93f
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2024-11-05 09:52:39 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241105/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void star(int n){
+    for(int i=0; i<n; i++)
+    {
+        
+            cout<<"12345^&*()_"<<"\n";
+        
+    
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int num;
+    cin>>num;
+    star(num);
+
+    return 0;
+}
