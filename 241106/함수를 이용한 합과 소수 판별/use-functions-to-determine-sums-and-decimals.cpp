e398fa69ab73a0d72512677bb3b575de1c53a938
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2024-11-06 20:25:04 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241106/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+bool prim(int n)
+{
+    if(n==1){return n;}
+    for(int j=2; j<n; j++)
+    {
+        if(n%j==0)
+        {
+            return 0;
+        }
+    
+    }
+
+    return 1;
+}
+
+bool even(int m)
+{
+    if(((m/10) +(m%10))%2==0)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a,b;
+    cin>>a>>b;
+
+    int cnt=0;
+
+    for(int i=a; i<b; i++)
+    {
+        if(prim(i) && even(i))
+        {
+            cnt++;
+        }
+    }
+    cout<<cnt;
+
+    return 0;
+}
