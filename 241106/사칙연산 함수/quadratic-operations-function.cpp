5776b8e5e0badb2693d6b14f4bdac4ad8b350bfe
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2024-11-06 20:06:48 +0900

    Title: 사칙연산 함수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241106/사칙연산 함수/quadratic-operations-function.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int func(int a, char b, int c)
+{
+    
+    if(b=='*')
+    {
+        return a*c;
+    }
+    else if(b=='+')
+    {
+        return a+c;
+    }
+    else if(b=='-')
+    {
+        return a-c;
+    }
+    else if (b=='/' && c!=0)
+    {
+        return a/c;
+    }
+
+    return false;
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a1,c1;
+    char b1;
+
+    cin>>a1>>b1>>c1;
+
+    
+    if(func(a1,b1,c1)==0)
+    {
+        cout<<"false";
+    }
+    else
+    {
+        cout<<a1<<" "<<b1<<" "<<c1<<" = "<<func(a1,b1,c1);
+    }
+
+
+
+
+    return 0;
+}
