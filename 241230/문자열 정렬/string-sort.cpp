f316a58242311730588607a744033563fa9171de
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2024-12-30 12:28:15 +0900

    Title: 문자열 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241230/문자열 정렬/string-sort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<algorithm>
+#include<string>
+#include<functional>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string s;
+    cin>>s;
+
+    sort(s.begin(), s.end());
+
+    cout<<s;
+
+
+    return 0;
+}
