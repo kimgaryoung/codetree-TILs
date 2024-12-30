3508e67b7bc04dd5bfbb13256c046467f05ce78d
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2024-12-30 20:46:23 +0900

    Title: 2개씩 그룹짓기, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 241230/2개씩 그룹짓기/group-of-pairs.cpp
+++ 241230/2개씩 그룹짓기/group-of-pairs.cpp
@@ -13,8 +13,28 @@ int main() {
         cin>>arr[i];
     }  
 
-    sort(arr,arr+n);
+    sort(arr,arr+2*n);
 
-    cout<<arr[0]+arr[n-1];
+
+
+    int max=0;
+    for(int j=0; j<n; j++)
+    {
+
+        int sum= arr[j]+arr[2*n-1-j];
+        
+        if(max < sum)
+        {
+            max=sum;
+        }
+        
+
+        
+    }
+
+    cout<<max;
+
     return 0;
+
+
 }
