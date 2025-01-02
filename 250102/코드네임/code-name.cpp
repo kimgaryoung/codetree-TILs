f1363187ed7083e70945974d42686ace64571952
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2025-01-02 22:49:55 +0900

    Title: 코드네임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250102/코드네임/code-name.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+class grade{
+
+    public:
+        char c;
+        int n;
+
+        grade(char c, int n)
+        {
+            this ->c=c;
+            this ->n=n;
+        }
+        grade(){}
+    
+};
+
+
+int main() {
+    // Please write your code here.
+
+    grade g1[5];
+    for(int i=0; i<5; i++)
+    {
+        char c;
+        int n;
+        cin>>c>>n;
+        g1[i]=grade(c,n);
+    }
+
+    
+
+    int min=0;
+    for(int i=1; i>5; i++)
+    {
+        if(g1[min].n>g1[i].n)
+            min =i;
+    }
+
+    cout<<g1[min].c<<" "<<g1[min].n;
+
+  
+
+    return 0;
+}
