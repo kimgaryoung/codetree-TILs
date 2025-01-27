34492d3b87064b270fec55bf66ce3d4235b69810
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2025-01-27 16:30:30 +0900

    Title: 원점으로부터의 거리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250127/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+
+class point 
+{
+    public: 
+        int x,y,num,k;
+
+    point( int k,int num)
+    {
+        
+        this -> num =num;
+        this -> k= k;
+
+    }
+    point(){}
+
+};
+
+bool cmp(point p, point q)
+{
+    return p.k<q.k;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    point arr[1000];
+
+    for(int i=0; i<n; i++)
+    {
+        int x,y;
+        cin>>x>>y;
+      
+        if(y<0)
+        {
+           y=y*(-1);
+        }
+        else if(x<0)
+        {
+            x=x*(-1);
+        }
+
+         int k=x+y;
+        
+        arr[i]=point(k,i+1);
+    }
+    
+    sort(arr,arr+n,cmp);
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i].num<<"\n";
+    }
+    // Please write your code here.
+    return 0;
+}
