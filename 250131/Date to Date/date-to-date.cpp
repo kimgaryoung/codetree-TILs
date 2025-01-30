b5219dec540b16e55772676930e5c9e73ca773bd
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2025-01-31 07:19:39 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250131/Date to Date/date-to-date.cpp
+++ 250131/Date to Date/date-to-date.cpp
@@ -1,13 +1,45 @@
 #include <iostream>
 using namespace std;
 
+int days(int m, int d)
+{
+     int day=0;
+
+    int arr[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+    for(int i=0; i<m; i++)
+    {
+        day+=arr[i];
+    }
+    day+=d;
+
+    return day;
+}
+
+
 int main() {
 
     int m1,d1,m2,d2;
 
     cin>>m1>>d1>>m2>>d2;
 
-    int day=1;
+   
+    cout<<days(m2,d2)-days(m1,d1)+1;
+   
+    // Please write your code here.
+    return 0;
+}
+
+/*
+int main() {
+
+    int m1,d1,m2,d2;
+
+    cin>>m1>>d1>>m2>>d2;
+
+    int day=0;
+
+    int arr[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
 
     while(true)
     {
@@ -16,31 +48,17 @@ int main() {
 
        
         d1++;
-        ++day;
+        day++;
 
 
-       if(d1>28)
+       if(day>arr[m1])
        { 
-        if(m1==1 ||m1==3 ||m1==5||m1==7||m1==8||m1==10||m1==12 && d1==31)
-        {
-             m1++;
-            d1=1;
-            day++;
-
-        }
-        else if(m1==4||m1==6||m1==9||m1==11 && d1==30)
-        {
+       
             m1++;
             d1=1;
-            day++;
+            
         }
-        else if(m1==2 &&d1==28)
-        {
-            m1++;
-            d1=1;
-            day++;
-        }
-       }
+       
 
 
     }
@@ -49,3 +67,4 @@ int main() {
     // Please write your code here.
     return 0;
 }
+*/
