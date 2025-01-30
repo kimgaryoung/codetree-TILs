c1537e84196f870bcea6d1fc13f6a6b113a021ba
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2025-01-31 06:58:56 +0900

    Title: Date to Date, Time: 1042ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 250131/Date to Date/date-to-date.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int m1,d1,m2,d2;
+
+    cin>>m1>>d1>>m2>>d2;
+
+    int day=1;
+
+    while(true)
+    {
+        if(m1==m2 && d1==d2)
+            break;
+
+       
+        d1++;
+        ++day;
+
+        if(m1==1 ||m1==3 ||m1==5||m1==7||m1==8||m1==10||m1==12 && d1==31)
+        {
+             m1++;
+            d1=1;
+            day++;
+
+        }
+        else if(m1==4||m1==6||m1==9||m1==11 && d1==30)
+        {
+            m1++;
+            d1=1;
+            day++;
+        }
+        else if(m1==2 &&d1==28)
+        {
+            m1++;
+            d1=1;
+            day++;
+        }
+
+
+    }
+
+    cout<<day;
+    // Please write your code here.
+    return 0;
+}
