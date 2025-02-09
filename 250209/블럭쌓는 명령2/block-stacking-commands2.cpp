e46b9848e2e47178f6106beeff08a79e29ec9720
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2025-02-09 17:48:12 +0900

    Title: 블럭쌓는 명령2, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250209/블럭쌓는 명령2/block-stacking-commands2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+    
+
+    for(int i=0; i<n; i++)
+    {
+        arr[i]=0;
+    }
+
+    int a,b;
+
+    for(int i=0; i<k; i++)
+    {
+        cin>>a>>b;
+        for(int j=a-1; j<b; j++)
+        {
+            arr[j]=arr[j]+1;
+        }
+    }
+
+    sort(arr,arr+k);
+
+   
+        cout<<arr[k-1];
+    
+    // Please write your code here.
+    return 0;
+}
