770d4214ba6bdcf8b015e5e01c52e5862a3b415a
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2025-02-16 21:25:52 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250216/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n; 
+    cin>>n; 
+
+    int a[1000];
+    int b[1000];
+
+    int cur=0;
+
+    int arr[2000]={0};
+
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        char c;
+
+        cin>>x>>c;
+        
+        
+
+        if(c=='R')
+        {
+           a[i]=cur;
+           b[i]=cur+x;
+
+           cur=cur+x;
+            
+            
+        }
+        else if(c=='L')
+        {  
+            a[i]=cur-x;
+            b[i]=cur;
+
+           cur=cur-x;
+
+           
+        } 
+
+            a[i]=a[i]+1000;
+            b[i]=b[i]+1000;
+
+
+    }
+
+
+
+
+    for(int i=0; i<n; i++)
+    {   
+       
+        for(int j=a[i]; j<b[i]; j++)
+        {
+            arr[j]++;
+
+        }
+        
+
+    }
+
+
+  
+
+
+
+
+    int count=0; 
+    for(int i=0; i<2000; i++)
+    {
+        if(arr[i]>=2)
+        {
+            count++;
+
+           
+        }
+    }
+    
+    cout<< count; 
+    
+    // Please write your code here.
+    return 0;
+}
