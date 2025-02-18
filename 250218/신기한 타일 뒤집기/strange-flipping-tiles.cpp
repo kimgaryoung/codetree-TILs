a7d07a8ab3a7de8f256bc24ade09b052e85001c0
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2025-02-18 14:54:19 +0900

    Title: 신기한 타일 뒤집기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250218/신기한 타일 뒤집기/strange-flipping-tiles.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+
+    int n;
+    cin>>n; 
+
+    int x;
+    char c;
+
+    
+
+   char arr[200001]={'G'};
+   int a[100000];
+   int B[100000];
+   
+    int cur=0;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>x>>c;
+
+
+        if(c=='R')
+        {
+            a[i]=cur+10001;
+            B[i]=cur+x+10000;
+            for(int j=a[i]; j<B[i]; j++)
+            {
+                arr[j]='B';
+            }
+
+              
+            cur=cur+x;
+            
+            
+
+        }
+        else
+        {
+
+            
+            a[i]=cur+10000;
+            B[i]=cur+10000-x;
+            for(int j=a[i]; j>B[i]; j--)
+            {
+                arr[j]='W';
+            }
+
+            cur=cur-x;
+
+        }
+
+
+      
+    }
+
+    int  b=0,w=0;
+
+    for(int i=0; i<200001; i++)
+    {
+        if(arr[i]=='W')
+        {
+            w++;
+
+
+        }
+        else if(arr[i]=='B')
+        {
+            b++;
+        }
+    }
+
+
+
+    cout<<w<<" "<<b;
+    return 0;
+}
