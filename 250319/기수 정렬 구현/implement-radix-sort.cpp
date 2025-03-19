024bcb2db2f8a7f48d076b57f4438b096146da9e
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2025-03-19 15:43:27 +0900

    Title: 기수 정렬 구현, Time: 34ms, Memory: 1MB, Status: Passed - CodeTree

--- 250319/기수 정렬 구현/implement-radix-sort.cpp
+++ 250319/기수 정렬 구현/implement-radix-sort.cpp
@@ -1,49 +1,44 @@
 #include <iostream>
-#include<vector>
+#include <vector>
+
+#define MAX_N 100000
+#define MAX_K 6
+#define MAX_DIGIT 10
+
 using namespace std;
 
+// 변수 선언
+int n;
+int arr[MAX_N];
+
+void RadixSort() {
+    int p = 1;
+    for(int pos = 0; pos < MAX_K; pos++) {
+        vector<int> arr_new[MAX_DIGIT];
+        for(int i = 0; i < n; i++) {
+            int digit = (arr[i] / p) % 10;
+            arr_new[digit].push_back(arr[i]);
+        }
+
+        int index = 0;
+        for(int i = 0; i < MAX_DIGIT; i++)
+            for(int j = 0; j < (int) arr_new[i].size(); j++)
+                arr[index++] = arr_new[i][j];
+        
+        p *= 10;
+    }
+}
+
 int main() {
-    // Please write your code here.
-    int n;
-    cin>>n; 
+    // 입력
+    cin >> n;
 
-    int arr[100000];
-
-    vector<int>v[10]; 
-
-    for(int i=0; i<n; i++)
-    {
-        cin>>arr[i];
-    }
-
-    int p=1; // 지금 시작 자릿수=> 1의 자리
-    int m=6;
-
-    for(int pos=0; pos<m; pos++)
-    {
-        for(int i=0; i<n; i++)
-        {
-            int d=(arr[i]/p)%10; // 특정 자릿수 비교를 위해 d에 넣기 
-
-            v[d].push_back(arr[i]); 
-        }
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
     
+    RadixSort();
 
-        int index =0; 
-        for(int i=0; i<10; i++)
-        {
-            for(int j=0; j<(int)v[i].size(); j++)
-                arr[index++]=v[i][j];
-        }
-
-        p*=10;
-    }
-
-
-for(int i=0; i<n; i++)
-{
-    cout<<arr[i]<<" ";
-
-}    
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
     return 0;
 }
