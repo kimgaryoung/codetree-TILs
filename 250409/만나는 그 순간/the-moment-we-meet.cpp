d803368d64700d8ba3621c7106c5dc8e70f2ce4b
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2025-04-09 14:52:16 +0900

    Title: 만나는 그 순간, Time: 9ms, Memory: 2MB, Status: Passed - Codetree

---
+++ 250409/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n,m;
+
+    cin>>n>>m;
+
+    int a[1000000];
+    int b[1000000];
+
+    int t=0;
+    char d=' ';
+    
+    /*
+    int pos=1; 
+    for(int i=0; i<n; i++)
+    {
+        cin>>d>>t;
+
+       
+        if(d=='R')
+        {
+
+            int tmp=0;
+            while(t--)
+            {
+            
+
+                a[pos]=a[pos+1]+1;
+                pos++;
+
+            }
+        }
+        else
+        {
+            
+            while(t--)
+            {
+            
+                a[pos]=a[pos-1]-1;
+                pos++;
+
+                
+            }
+        }
+    }
+
+    pos=1;
+    t=0;
+    d=' ';
+    for(int j=0; j<m; j++)
+    {
+
+        cin>>d>>t;
+        
+        if(d=='R')
+        {
+
+            int tmp=0;
+            while(t--)
+            {
+        
+                b[pos]=b[pos+1]+1;
+                pos++;
+
+            }
+        }
+        else
+        {
+            
+            while(t--)
+            {
+            
+                b[pos]=b[pos-1]-1;
+
+                pos++;
+
+                
+            }
+        }
+    }
+*/
+
+    int pos=1;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>d>>t;
+
+        while(t--)
+        {
+            if(d=='R')
+            {
+                a[pos]=a[pos-1]+1;
+            }
+            else
+            {
+                a[pos]=a[pos-1]-1;
+            }
+
+            pos++;
+        }
+        
+    }
+
+
+    pos=1;
+    d=' ';
+    t=0;
+    for(int i=0; i<m; i++)
+    {
+        cin>>d>>t;
+
+        while(t--)
+        {
+            if(d=='R')
+            {
+                b[pos]=b[pos-1]+1;
+            }
+            else
+            {
+                b[pos]=b[pos-1]-1;
+            }
+            pos++;
+        }
+    }
+    
+    int ans=-1;
+
+    //cout<<pos;
+    for(int i=1; i<pos; i++)
+    {
+
+        //cout<<a[i]<<" "<<b[i]<<"\n";
+        if(a[i]==b[i] )
+        {
+            ans=i;
+            break;
+        }
+        
+        
+
+
+    }
+
+    cout<<ans;
+    return 0;
+}
