1c9828025bb8faf290d2a21d41f5377cc0c699ab
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2025-09-16 08:39:22 +0900

    Title: 되돌아오기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250916/되돌아오기/come-back.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int num=0;
+
+int arr[1000][1000]={};
+
+
+
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+
+
+
+
+int main() {
+    // Please write your code here.
+
+    cin>>N;
+    int m;
+    char c;
+
+    int nx,ny;
+
+   
+    int num=1;
+   
+    
+
+    int x=0,y=0;
+
+    for(int i=0; i<N; i++)
+    {
+        
+        cin>>c>>m;
+
+        if(arr[0][0]==0){
+        if(c=='N' )
+        {
+            for(int i=0; i<m; i++)
+            {
+                x=x+dx[0];
+                y=y+dy[0];
+                arr[x][y]=num++;
+
+               
+            }
+        }
+        else if (c=='S' )
+        {
+            for(int i=0; i<m; i++)
+            {
+                x=x+dx[2];
+                y=y+dy[2];
+                arr[x][y]=num++;
+
+            }
+        }
+        else if(c=='W')
+        {
+            for(int i=0; i<m; i++)
+            {
+                x=x+dx[3];
+                y=y+dy[3];
+                arr[x][y]=num++;
+
+            }
+        }
+        else 
+        {
+            for(int i=0; i<m; i++)
+            {
+                x=x+dx[1];
+                y=y+dy[1];
+                arr[x][y]=num++;
+            }
+
+        }
+        }
+        else 
+        {
+            cout<<arr[0][0];
+            break;
+        }
+
+       
+        
+
+        
+    }
+
+    if (arr[0][0]==0)
+    {
+  
+        cout<<"-1";
+    }
+
+
+
+    
+    return 0;
+}
