c43e2c0039686a020399fd62048c066ca699d3af
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2025-09-16 09:30:38 +0900

    Title: 되돌아오기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250916/되돌아오기 2/come-back-2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int arr[10001][10001]={};
+
+const int fn=5000;
+
+
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+
+int main() {
+
+    int dir=0;
+    char c;
+
+    int x=fn,y=fn; 
+    int num=1;
+
+    while(cin>>c)// 입력이 있을때 까지 수행.
+    {
+        
+        
+            if(c=='F')
+            {
+                x=x+dx[dir];
+                y=y+dy[dir];
+
+                if (arr[fn][fn]!= 0)
+                {
+                    break;
+                }
+            
+                arr[x][y] = num++;
+                //cout<<"x"<<x<<"y"<<y<<"arr값"<<num<<"\n";
+                
+            }
+            else if(c=='L')
+            {
+                dir=(dir-1)%4;
+                num++;
+
+            }
+            else if(c=='R')
+            {
+                dir=(dir-1+4)%4;
+                num++;
+            }
+            
+
+        }
+        
+    
+        if(arr[fn][fn]!=0)
+        {
+            cout<<arr[fn][fn];
+        }
+        else
+        {
+            cout<< -1;
+        }
+    
+    
+
+    // Please write your code here.
+    return 0;
+}
