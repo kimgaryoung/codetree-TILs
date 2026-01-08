b4481ee928e66e6c8248e5e813a9b6f0bb1ca253
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2026-01-08 15:47:32 +0900

    Title: 바구니 안의 사탕 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260108/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include<algorithm> // max 사용 
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n,k;
+    cin>>n>>k;
+
+    int arr[101]={0};
+
+    
+
+
+    int v,idx;
+    int m_idx=0;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>v>>idx;
+
+        arr[idx]=v;
+
+        m_idx =max(idx,m_idx);
+
+    }
+
+    int sum=0;
+    int cor_a=0;
+    for(int i=k; i<m_idx-k; i++)// 중심
+    {
+        sum=0;
+        for(int j=0; j<=i+k; j++) // 더할 범위 
+        {
+            sum+=arr[j];
+
+        }
+
+        cor_a=max(cor_a,sum);
+
+
+
+    }
+
+    cout<<cor_a;
+
+
+
+   
+
+
+
+
+
+
+
+    return 0;
+}
