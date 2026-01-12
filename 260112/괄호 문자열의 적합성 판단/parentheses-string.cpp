3c52ae4c3a2ed788538a805025fbf01007e80a8f
Hansol2124 <123079326+kimgaryoung@users.noreply.github.com>
2026-01-12 16:52:58 +0900

    Title: 괄호 문자열의 적합성 판단, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260112/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include<stack>
+
+using namespace std;
+
+string str;
+
+int main() {
+    cin >> str;
+
+    stack<char>s;
+
+    for(int i=0; i<str.length(); i++)
+    {
+        if(str[i]=="(")
+        {
+            s.push('(')
+        }
+        else 
+        {
+            if(s.empty)
+            {
+                cout<<"No";
+                return 0;
+            }
+       
+
+        s.pop();
+        }
+    }
+
+    if(!s.empty)
+    {
+        cout<<"No";
+    }
+    else 
+    {
+        cout<<"Yes";
+    }
+
+
+
+
+
+    // Please write your code here.
+
+    return 0;
+}
