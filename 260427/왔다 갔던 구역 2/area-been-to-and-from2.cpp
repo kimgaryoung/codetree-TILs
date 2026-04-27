2809708a3f22adf70790ebec32aa0db52a1b8b96
Garyeong <123079326+kimgaryoung@users.noreply.github.com>
2026-04-27 18:04:09 +0900

    Title: 왔다 갔던 구역 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260427/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[1001];
+    for(int i=0; i<1001; i++)
+    {
+        arr[i]=0;
+    }
+
+    int n;
+    int cur=500;
+    cin>>n; 
+
+    for(int i=0; i<n; i++)// 명령어 1개
+    {
+        int num;
+        char cmd;
+        cin>> num>>cmd;
+
+        for(int j=0; j<num; j++)
+        {
+            if(cmd=='L')// 문자열이랑 문자에서 ""와 ''구분 때문에 시간 뺏김
+            {
+                arr[cur--]++;
+                // 이렇게 하면 cur도 값이 변하나? 
+            }
+            else if(cmd=='R')
+            {
+                arr[cur++]++;
+
+            }
+
+        }
+
+    }      
+
+
+    int anw=0;
+
+    for(int i=0; i<1001; i++)
+    {
+        if(arr[i]>=2)
+        {
+            anw++;
+        }
+
+    }
+
+    cout<<anw;
+
+
+
+
+    // Please write your code here.
+    return 0;
+}
