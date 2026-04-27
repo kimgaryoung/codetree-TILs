c38d5fa250d820039e01d0e38e65f55d6e87a8cd
Garyeong <123079326+kimgaryoung@users.noreply.github.com>
2026-04-27 18:18:17 +0900

    Title: 왔다 갔던 구역 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260427/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 260427/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -11,28 +11,44 @@ int main() {
 
     int n;
     int cur=500;
+    arr[500]=1;
     cin>>n; 
 
     for(int i=0; i<n; i++)// 명령어 1개
     {
         int num;
         char cmd;
-        cin>> num>>cmd;
+        cin>>num>>cmd;
 
-        for(int j=0; j<=num; j++)
+        for(int j=0; j<num; j++)
         {
+            arr[cur]++;
+            
             if(cmd=='L')// 문자열이랑 문자에서 ""와 ''구분 때문에 시간 뺏김
             {
-                arr[cur--]++;
+                --cur;
                 // 이렇게 하면 cur도 값이 변하나? 
             }
             else if(cmd=='R')
             {
-                arr[cur++]++;
+                ++cur;
+                
 
             }
 
         }
+        
+        /*
+        if(cmd=='L')
+        {
+            cur+=1;
+        }
+        else
+        {
+            cur-=1;
+        }
+        */
+        
 
 
     }      
@@ -45,6 +61,8 @@ int main() {
         if(arr[i]>=2)
         {
             anw++;
+
+            //cout<<i-500<<"반쪠"<<"\n"; // 처음 인텍스가  cur이 적용이 안되는 문제인 걸 알아냄.
         }
 
     }
