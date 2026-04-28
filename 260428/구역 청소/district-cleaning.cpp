1e744f41e2eff124060083cfb394daccac8875fc
Garyeong <123079326+kimgaryoung@users.noreply.github.com>
2026-04-28 14:01:36 +0900

    Title: 구역 청소, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260428/구역 청소/district-cleaning.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a,b,c,d;
+
+    cin>>a>>b>>c>>d;
+
+    int arr[100];
+    
+    for(int i=0; i<100; i++)
+    {
+        arr[i]=0;
+
+    }
+
+    for(int i=a; i<=b; i++)
+    {
+        arr[i]++;
+
+    }
+
+    for(int i=c; i<=d; i++)
+    {
+        arr[i]++;
+
+    }
+
+
+
+    bool clean1=false;
+    int cnt=0;
+
+    for(int i=0; i<100; i++)
+    {
+        if(arr[i]!=0)
+        {
+            cnt++;
+        }
+         if(arr[i]==2)
+        {
+            clean1=true;
+        }
+
+       
+
+    }
+
+
+
+    if(clean1)// 겹침
+    {
+        cout<<cnt-1;
+    }
+    else
+    {
+        cout<<cnt-2;
+    }
+    // Please write your code here.
+    return 0;
+}
